d7101760335e6016f1595fa9c96b7d9dd9b71527
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2024-03-02 10:39:03 -0300

    feat: new lsp config (for fennel and lua)

--- turing/.config/nvim/lua/plugins/theme.lua
+++ turing/.config/nvim/lua/plugins/theme.lua
@@ -8,7 +8,7 @@ local function _2_()
   local function _3_(colors)
     return {Normal = {bg = "#0D1014", fg = "#DCD7BA"}, FoldColumn = {bg = "#16161D"}, SignColumn = {bg = "#16161D"}, LineNr = {bg = "#16161D"}, CursorColumn = {bg = "#16161D"}, CursorLine = {bg = "#16161D"}, CursorLineNr = {bg = "#16161D"}, DiagnosticSignError = {bg = "#16161D"}, DiagnosticSignWarn = {bg = "#16161D"}, DiagnosticSignInfo = {bg = "#16161D"}, DiagnosticSignHint = {bg = "#16161D"}}
   end
-  theme.setup({undercurl = true, commentStyle = {italic = true}, functionStyle = {}, keywordStyle = {italic = true}, statementStyle = {bold = true}, typeStyle = {}, terminalColors = true, colors = {palette = {}, theme = {wave = {}, lotus = {}, dragon = {}, all = {}}}, overrides = _3_, theme = "wave", background = {dark = "wave", light = "lotus"}, dimInactive = false, transparent = false, compile = false})
+  theme.setup({undercurl = true, commentStyle = {italic = true}, functionStyle = {}, keywordStyle = {italic = true}, statementStyle = {bold = true}, typeStyle = {}, terminalColors = true, colors = {palette = {}, theme = {wave = {}, lotus = {}, dragon = {}, all = {}}}, overrides = _3_, theme = "wave", background = {dark = "wave", light = "lotus"}, transparent = false, compile = false, dimInactive = false})
   return vim.cmd("colorscheme kanagawa")
 end
 return {{"rebelot/kanagawa.nvim", priority = 1000, dependencies = {"nvim-tree/nvim-web-devicons"}, config = _2_, lazy = false}}
